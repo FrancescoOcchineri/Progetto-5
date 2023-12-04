7f05e0918689f05550c153d28fef43b0a75c8fa5
FrancescoOcchineri <occhinerifrancesco@gmail.com>
2023-12-04 19:11:14 +0100

    Progetto 5

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let editProfile = document.querySelector(".edit");
+    editProfile.addEventListener("click", function () {
+        window.location.href = "editProfile.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    let cancel = document.querySelector(".cancel");
+    cancel.addEventListener("click", function () {
+        window.location.href = "index.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    let save = document.querySelector(".save");
+    save.addEventListener("click", function () {
+        window.location.href = "index.html";
+    });
+});
+
